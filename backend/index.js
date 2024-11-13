e563d939aa3a6d4705637f586293e1bcfca10602
Sᗩᑘᖇᗩᗷᕼ <134703565+Saurabh-Shrivastav@users.noreply.github.com>
2024-11-13 16:50:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: 'https://tata-earning.vercel.app', // अपने फ्रंटेंड का URL यहां डालें
+}));
 
 // MongoDB connection
 // mongoose.connect("mongodb://127.0.0.1:27017/mern-auth").then(async () => {
