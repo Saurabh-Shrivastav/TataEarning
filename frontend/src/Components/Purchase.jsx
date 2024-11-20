8530dc7e87b18294c607582eea5dc7a5175a308c
Sᗩᑘᖇᗩᗷᕼ <134703565+Saurabh-Shrivastav@users.noreply.github.com>
2024-11-20 08:45:40 +0530

    Update Purchase.jsx

--- frontend/src/Components/Purchase.jsx
+++ frontend/src/Components/Purchase.jsx
@@ -9,7 +9,7 @@ function Purchase() {
 
     // Fetch confirmed purchases
     useEffect(() => {
-        fetch('http://localhost:4000/purchases')
+        fetch('https://tataearning.onrender.com/purchases')
             .then((response) => response.json())
             .then((data) => {
                 setPurchases(data);
